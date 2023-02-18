64b9825968e4da003f3f540571504442d93f978a
LapertJoseph <lapertjoseph@gmail.com>
2023-02-18 01:19:14 +0100

    add elevatedButton with function print

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 
-//https://github.com/LapertJoseph/FlutterApp
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer choosen!');
+  }
+
   @override // good practice to override return type here
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s is your favorite color?',
+      'What\'s is your favorite animals?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text("This is my default text!"),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 choosen!'),
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                // do something
+                print('Answer 3 choosen!');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
